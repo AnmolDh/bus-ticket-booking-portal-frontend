88807ea379bb67759fc412893fe2b5c09e9ad9ab
Anmol Dhiman <73706789+AnmolDh@users.noreply.github.com>
2025-06-11 15:41:58 +0530

    api client

---
+++ frontend/src/main/java/com/team4/frontend/client/ApiClient.java
@@ -0,0 +1,13 @@
+package com.team4.frontend.client;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Service
+@RequiredArgsConstructor
+public class ApiClient {
+
+    private final WebClient webClient;
+
+}
