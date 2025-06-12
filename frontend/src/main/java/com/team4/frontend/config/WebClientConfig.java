d1e71efe76f4d13b61c6d2f07c0c98b0b32d7e54
Anmol Dhiman <73706789+AnmolDh@users.noreply.github.com>
2025-06-12 09:24:02 +0530

    added apiclient

--- frontend/src/main/java/com/team4/frontend/config/WebClientConfig.java
+++ frontend/src/main/java/com/team4/frontend/config/WebClientConfig.java
@@ -2,7 +2,9 @@ package com.team4.frontend.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.netty.http.client.HttpClient;
 
 @Configuration
 public class WebClientConfig {
@@ -11,6 +13,11 @@ public class WebClientConfig {
     public WebClient webClient() {
         return WebClient.builder()
                 .baseUrl("http://localhost:8080/api")
+                .codecs(configurer -> configurer
+                        .defaultCodecs()
+                        .maxInMemorySize(10 * 1024 * 1024)
+                )
+                .clientConnector(new ReactorClientHttpConnector(HttpClient.create()))
                 .build();
     }
 }
